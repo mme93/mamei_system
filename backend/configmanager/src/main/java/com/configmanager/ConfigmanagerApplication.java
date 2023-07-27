34319590a7dd1fc787385e1b4dd47e93671ab3b4
magantus@googlemail.com <Extrem93!>
2023-07-27 23:39:51 +0200

    services init

---
+++ backend/configmanager/src/main/java/com/configmanager/ConfigmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.configmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigmanagerApplication.class, args);
+	}
+
+}
