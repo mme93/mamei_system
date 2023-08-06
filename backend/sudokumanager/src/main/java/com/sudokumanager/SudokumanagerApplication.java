245686251f2db9b526043090bdd6089f179d0021
magantus@googlemail.com <Extrem93!>
2023-08-07 00:39:45 +0200

    update

---
+++ backend/sudokumanager/src/main/java/com/sudokumanager/SudokumanagerApplication.java
@@ -0,0 +1,13 @@
+package com.sudokumanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokumanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudokumanagerApplication.class, args);
+	}
+
+}
