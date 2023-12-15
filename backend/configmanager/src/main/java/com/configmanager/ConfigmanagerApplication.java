67ae0a005fd120160eeea29daeed71403498c32b
magantus@googlemail.com <Extrem93!>
2023-12-15 15:45:18 +0100

    clean up

--- backend/configmanager/src/main/java/com/configmanager/ConfigmanagerApplication.java
+++ backend/configmanager/src/main/java/com/configmanager/ConfigmanagerApplication.java
@@ -2,10 +2,9 @@ package com.configmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class ConfigmanagerApplication {
 
 	public static void main(String[] args) {
