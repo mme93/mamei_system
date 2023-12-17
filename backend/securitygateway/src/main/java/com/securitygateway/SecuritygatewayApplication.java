34a64012e32ff47e2bb5c46c132cfde93afb5e58
magantus@googlemail.com <Extrem93!>
2023-12-17 14:28:14 +0100

    update

---
+++ backend/securitygateway/src/main/java/com/securitygateway/SecuritygatewayApplication.java
@@ -0,0 +1,13 @@
+package com.securitygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritygatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritygatewayApplication.class, args);
+	}
+
+}
---
+++ backend/securitygateway/src/test/java/com/securitygateway/SecuritygatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritygatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
