08e9c3a9abda43ed87b38fdb35b5b0b1d27b35d4
magantus@googlemail.com <Extrem93!>
2023-12-26 20:32:28 +0100

    update

--- frontend/src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'Ich bin kein Docker (Development-Umgebung)'
-};
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
   apiUrl: 'Ich bin die normale Env'
 };
