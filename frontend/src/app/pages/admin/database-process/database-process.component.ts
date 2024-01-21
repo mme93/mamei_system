02f10c066adb3df0cdae252a154a76b949ae9bfe
magantus@googlemail.com <Extrem93!>
2024-01-21 23:14:25 +0100

    update

--- frontend/src/app/pages/admin/database-process/database-process.component.ts
+++ frontend/src/app/pages/admin/database-process/database-process.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {MatCheckboxChange} from "@angular/material/checkbox";
 
 export interface DatabaseProcess {
   id: number;
@@ -6,6 +7,7 @@ export interface DatabaseProcess {
   processIcon: string;
   processDescription: string;
   processActivated: boolean;
+  processIsShowActivated: boolean;
   processStatusIcon: string;
 }
 
@@ -15,43 +17,81 @@ export interface DatabaseProcess {
   styleUrls: ['./database-process.component.scss']
 })
 export class DatabaseProcessComponent implements OnInit {
-
-  incr = 1;
-  processStatusIcon = ['play_circle_filled', 'build', 'done', 'error'];
+  default='Choose your steps for Database Processes';
+  incr = 0;
+  processStatusIcon = ['play_disabled', 'play_arrow', 'build', 'done', 'error'];
 
   processList: DatabaseProcess[] = [
     {
       id: 1,
-      processLabel: 'Delete Database',
+      processLabel: 'BackUp Database-Data',
       processIcon: 'info',
-      processDescription: 'Delete Database with all tables and data.',
+      processDescription: 'Create a BackUp for this Database.',
       processActivated: false,
+      processIsShowActivated: true,
       processStatusIcon: this.processStatusIcon[0]
     }, {
       id: 2,
-      processLabel: 'Build Table',
+      processLabel: 'Delete Database mamei_system',
       processIcon: 'info',
       processDescription: 'Delete Database with all tables and data.',
       processActivated: false,
+      processIsShowActivated: true,
       processStatusIcon: this.processStatusIcon[0]
     }, {
       id: 3,
+      processLabel: 'Create Database mamei_system',
+      processIcon: 'info',
+      processDescription: 'Restart all Micro-Services, to create Tables.',
+      processActivated: false,
+      processIsShowActivated: true,
+      processStatusIcon: this.processStatusIcon[0]
+    }, {
+      id: 4,
+      processLabel: 'Restart all needed Micro-Services and create Table',
+      processIcon: 'info',
+      processDescription: 'Restart all Micro-Services, to create Tables.',
+      processActivated: false,
+      processIsShowActivated: true,
+      processStatusIcon: this.processStatusIcon[0]
+    }, {
+      id: 5,
       processLabel: 'Set Default Dataset',
       processIcon: 'info',
       processDescription: 'Create default data in different tables, which needed for the system.',
       processActivated: false,
+      processIsShowActivated: true,
       processStatusIcon: this.processStatusIcon[0]
     }
   ]
+  currentProcess = 1;
   progress = 0;
-  isloading = false;
+  isLoading = false;
+  itemText = this.default;
 
   ngOnInit(): void {
   }
 
   startProcess(): void {
-    this.progress += 50;
-    this.isloading = true;
+    this.isLoading = true;
+    console.log(this.processList)
+    this.processList.forEach(process => {
+      process.processIsShowActivated = process.processActivated;
+      if (process.processActivated) {
+        process.processStatusIcon=this.processStatusIcon[2];
+        this.incr++;
+      }
+    });
+    this.itemText ='Database Processes ' +this.currentProcess + '/' + this.incr+' in progress.';
+
   }
 
+  changeIcon(index: number, $event: MatCheckboxChange) {
+    if ($event.checked) {
+      this.processList[index].processStatusIcon = this.processStatusIcon[1];
+    } else {
+      this.processList[index].processStatusIcon = this.processStatusIcon[0];
+    }
+    this.processList[index].processActivated = $event.checked;
+  }
 }
