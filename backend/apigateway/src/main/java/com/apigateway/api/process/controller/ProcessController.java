3f5d9ed90d1ddb48836151d0dd28781cd3dde454
magantus@googlemail.com <Extrem93!>
2024-01-23 18:39:13 +0100

    update

--- backend/apigateway/src/main/java/com/apigateway/api/process/controller/ProcessController.java
+++ backend/apigateway/src/main/java/com/apigateway/api/process/controller/ProcessController.java
@@ -1,10 +1,12 @@
 package com.apigateway.api.process.controller;
 
+import com.apigateway.api.process.model.SystemProcess;
 import com.apigateway.api.process.service.ProcessService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,7 @@ public class ProcessController {
 
 
     @PostMapping("/newJob")
-    public ResponseEntity<String> startNewJob(){
+    public ResponseEntity<String> startNewJob(@RequestBody SystemProcess process){
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
