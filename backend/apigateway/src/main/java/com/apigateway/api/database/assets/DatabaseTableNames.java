2b9e96332b20b5def522e901d413b1021235ed99
magantus@googlemail.com <Extrem93!>
2024-01-31 17:55:26 +0100

    update

--- backend/apigateway/src/main/java/com/apigateway/api/database/assets/DatabaseTableNames.java
+++ backend/apigateway/src/main/java/com/apigateway/api/database/assets/DatabaseTableNames.java
@@ -6,10 +6,32 @@ import static java.util.Arrays.asList;
 
 public class DatabaseTableNames {
 
-    public static final String PROCESS="process";
+    public static final String PROCESS = "process";
+    public static final String ACCOUNT = "account";
+    public static final String ACCOUNT_SEQ = "account_seq";
+    public static final String DEPENDED_PROCESS = "depended_process";
+    public static final String MICRO_SERVICE = "micro_service";
+    public static final String PRIVILEGES = "privileges";
+    public static final String PRIVILEGES_SEQ = "privileges_seq";
+    public static final String SECURITY_USER = "security_user";
+    public static final String SUDOKU = "sudoku";
+    public static final String SUDOKU_LEVEL_LIST_ITEM = "sudoku_level_list_item";
+    public static final String TASK = "task";
 
-    public static final List<String>tableNameList= asList(PROCESS);
+    public static final List<String> tableNameList = asList(PROCESS, ACCOUNT, ACCOUNT_SEQ, DEPENDED_PROCESS,
+            MICRO_SERVICE, PRIVILEGES, PRIVILEGES_SEQ, SECURITY_USER, SUDOKU, SUDOKU_LEVEL_LIST_ITEM, TASK);
 
-    public static final String tableNames="process";
+    public static final String tableNames = "account, account_seq, depended_process, micro_service, privileges, " +
+            "privileges_seq, process, security_user, sudoku, sudoku_level_list_item, task";
 
 }
+
+
+
+
+
+
+
+
+
+
