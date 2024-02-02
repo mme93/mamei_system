1bfba82fad1bd01be9ac7790907eaf7d46c45c7a
magantus@googlemail.com <Extrem93!>
2024-02-02 18:24:37 +0100

    update

--- backend/datastoragemanager/src/main/java/com/datastoragemanager/webdav/service/WebDavService.java
+++ backend/datastoragemanager/src/main/java/com/datastoragemanager/webdav/service/WebDavService.java
@@ -1,11 +1,13 @@
 package com.datastoragemanager.webdav.service;
 
+import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class WebDavService {
@@ -20,24 +22,28 @@ public class WebDavService {
     private String url;
 
     public boolean createFolder(String path) {
-        try{
+        try {
             Sardine sardine = SardineFactory.begin(this.user, this.password);
-           // Sardine sardine = SardineFactory.begin(null,null);
-            sardine.createDirectory(this.url+path);
+            List<DavResource> resources = sardine.list(this.url);
+            for (DavResource res : resources) {
+                System.err.println(res.getName());
+            }
+            sardine.createDirectory(this.url+"mamei/systems");
             return true;
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
     }
+
     public void createFile(String path) {
     }
 
-    public boolean fileExist(String path){
+    public boolean fileExist(String path) {
         return false;
     }
 
-    public boolean folderExist(String path){
+    public boolean folderExist(String path) {
         return false;
     }
 }
