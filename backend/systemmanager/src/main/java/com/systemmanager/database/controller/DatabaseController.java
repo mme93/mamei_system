e6b9f68518819251709837180e59f9f5ab08e124
magantus@googlemail.com <Extrem93!>
2024-02-02 16:42:05 +0100

    update

---
+++ backend/systemmanager/src/main/java/com/systemmanager/database/controller/DatabaseController.java
@@ -0,0 +1,9 @@
+package com.systemmanager.database.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/database")
+public class DatabaseController {
+}
---
+++ backend/systemmanager/src/main/java/com/systemmanager/database/service/DatabaseService.java
@@ -0,0 +1,7 @@
+package com.systemmanager.database.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DatabaseService {
+}
