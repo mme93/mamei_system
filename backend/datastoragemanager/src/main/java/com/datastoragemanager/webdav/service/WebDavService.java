1272e63a72a0ac06c0c7228ad352f095ecf71295
magantus@googlemail.com <Extrem93!>
2024-02-04 16:43:57 +0100

    update

--- backend/datastoragemanager/src/main/java/com/datastoragemanager/webdav/service/WebDavService.java
+++ backend/datastoragemanager/src/main/java/com/datastoragemanager/webdav/service/WebDavService.java
@@ -6,7 +6,11 @@ import com.github.sardine.SardineFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 @Service
@@ -28,7 +32,7 @@ public class WebDavService {
             for (DavResource res : resources) {
                 System.err.println(res.getName());
             }
-            sardine.createDirectory(this.url+"mamei/systems");
+            sardine.createDirectory(this.url+"mamei/");
             return true;
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,7 +40,34 @@ public class WebDavService {
         }
     }
 
+    public void loadFileToWebDav(){
+        try {
+            Sardine sardine = SardineFactory.begin();
+            sardine.setCredentials(this.user, this.password);
+            File localFile = x();
+            System.err.println(localFile.exists());
+           if(localFile.exists()){
+               FileInputStream fileInputStream = new FileInputStream(localFile);
+               sardine.put("http://212.227.165.166/webdav/anleitungs.txt" , fileInputStream.readAllBytes());
+               fileInputStream.close();
+           }
+
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File x() throws URISyntaxException {
+        URL resource = getClass().getClassLoader().getResource("anleitung.txt");
+        if (resource == null) {
+            throw new IllegalArgumentException("file not found!");
+        } else {
+            return new File(resource.toURI());
+        }
+    }
+
     public void createFile(String path) {
+        loadFileToWebDav();
     }
 
     public boolean fileExist(String path) {
