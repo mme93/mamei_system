957bc740b577eb32b7cd0e0902a137e7cd47227a
magantus@googlemail.com <Extrem93!>
2024-02-18 20:42:27 +0100

    update

--- backend/massdatapool/src/main/java/com/massdatapool/MassdatapoolApplication.java
+++ backend/massdatapool/src/main/java/com/massdatapool/MassdatapoolApplication.java
@@ -1,13 +1,21 @@
 package com.massdatapool;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MassdatapoolApplication {
 
+	private static Logger LOGGER = LoggerFactory.getLogger(MassdatapoolApplication.class);
+	private static final Logger LOGGER_INFO = LoggerFactory.getLogger("com.example.info");
+
 	public static void main(String[] args) {
 		SpringApplication.run(MassdatapoolApplication.class, args);
+		LOGGER_INFO.info("This is information");
+		LOGGER.debug("This is debug");
+		LOGGER.error("This is error");
 	}
 
 }
