547606edb441e69f5d343f51974a59661085e331
magantus@googlemail.com <Extrem93!>
2024-02-18 16:31:37 +0100

    update

---
+++ backend/massdatapool/src/main/java/com/massdatapool/MassdatapoolApplication.java
@@ -0,0 +1,13 @@
+package com.massdatapool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassdatapoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassdatapoolApplication.class, args);
+	}
+
+}
---
+++ backend/massdatapool/src/test/java/com/massdatapool/MassdatapoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.massdatapool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassdatapoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
