7fbf4161697d9f67ae16e8dc2c5b3d65d6bc61b9
magantus@googlemail.com <Extrem93!>
2024-02-19 00:02:19 +0100

    update

--- frontend/src/app/pages/admin/logfiles/logfiles.component.ts
+++ frontend/src/app/pages/admin/logfiles/logfiles.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {LogfilesService} from "../../../shared/services/admin/logfiles/logfiles.service";
 
 @Component({
   selector: 'app-logfiles',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class LogfilesComponent {
 
+  constructor(private logfilesService: LogfilesService) {
+  }
+
+  downloadInfoLog() {
+    this.logfilesService.downloadInfoLog().subscribe((data: Blob) => {
+      const downloadLink = document.createElement('a');
+      const url = window.URL.createObjectURL(data);
+      downloadLink.href = url;
+      downloadLink.download = 'info.log';
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      setTimeout(() => {
+        document.body.removeChild(downloadLink);
+        window.URL.revokeObjectURL(url);
+      }, 100);
+    });
+  }
 }
--- frontend/src/app/shared/services/admin/logfiles/logfiles.service.ts
+++ frontend/src/app/shared/services/admin/logfiles/logfiles.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../../environments/environment";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Process} from "../../../model/admin/process/ProcessApiEntity";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogfilesService {
 
-  constructor() { }
+  private logInfoFileDownloadUrl = environment.uri + ':9000/api/massdatapool/log/info'
+
+  constructor(private http: HttpClient) {
+  }
+
+  downloadInfoLog(): Observable<Blob> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': localStorage.getItem('token') + '',
+        'Content-Type': 'application/json'
+      }),
+      responseType: 'blob' as 'json' // Important
+    };
+    return this.http.get<Blob>(this.logInfoFileDownloadUrl, httpOptions);
+  }
 }
