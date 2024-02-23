55714b723e79331131a8b045d3fde796b4baf5ad
magantus@googlemail.com <Extrem93!>
2024-02-23 23:20:53 +0100

    update

--- backend/apigateway/src/main/java/com/apigateway/api/eureka/service/DiscoveryClientService.java
+++ backend/apigateway/src/main/java/com/apigateway/api/eureka/service/DiscoveryClientService.java
@@ -5,62 +5,110 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service for interacting with Eureka Discovery Client.
+ */
 @Service
 @RequiredArgsConstructor
 public class DiscoveryClientService {
 
     private final DiscoveryClient discoveryClient;
 
-    public String getSudokuClientAdress(){
-       return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.SudokuAPI).get(0).getUri().toString();
+    /**
+     * Retrieves the address of the Sudoku client.
+     */
+    public String getSudokuClientAdress() {
+        return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.SudokuAPI).get(0).getUri().toString();
     }
 
-    public String getSystemClientAdress(){
+    /**
+     * Retrieves the address of the System client.
+     */
+    public String getSystemClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.SystemManagerAPI).get(0).getUri().toString();
     }
 
-    public String getSecurityGatewayClientAdress(){
+    /**
+     * Retrieves the address of the Gateway client.
+     */
+    public String getSecurityGatewayClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.SecurityGatewayAPI).get(0).getUri().toString();
     }
 
-    public String getUserClientAdress(){
+    /**
+     * Retrieves the address of the User client.
+     */
+    public String getUserClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.UserAPI).get(0).getUri().toString();
     }
 
-    public String getDashboardClientAdress(){
+    /**
+     * Retrieves the address of the Dashboard client.
+     */
+    public String getDashboardClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.DashboardAPI).get(0).getUri().toString();
     }
 
-    public String getMassDataPoolClientAdress(){
+    /**
+     * Retrieves the address of the MassDataPool client.
+     */
+    public String getMassDataPoolClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.MassDataPoolAPI).get(0).getUri().toString();
     }
 
-    public String getGamesManagerClientAdress(){
+    /**
+     * Retrieves the address of the Games Manager client.
+     */
+    public String getGamesManagerClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.GamesManager).get(0).getUri().toString();
     }
 
-    public String getMameieFsmClientAdress(){
+    /**
+     * Retrieves the address of the Mameie FSM client.
+     */
+    public String getMameieFsmClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.Mameie_FSM).get(0).getUri().toString();
     }
 
-    public String getDatastorageManagerClientAdress(){
+    /**
+     * Retrieves the address of the Datastorage Manager client.
+     */
+    public String getDatastorageManagerClientAdress() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.DataStorageAPI).get(0).getUri().toString();
     }
 
-    public String getHealtManagerAdressByName(){
+    /**
+     * Retrieves the address of the Health Manager client.
+     */
+    public String getHealtManagerAdressByName() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.HealthManagerAPI).get(0).getUri().toString();
     }
 
-    public String getShoppingListAdressByName(){
+    /**
+     * Retrieves the address of the Shopping List client.
+     */
+    public String getShoppingListAdressByName() {
         return discoveryClient.getInstances(EurekaDiscoveryClientNameTable.ShoppinglistAPI).get(0).getUri().toString();
     }
 
-    public String getClientAdressByName(String clientName){
+    /**
+     * Retrieves the address of a client by name.
+     *
+     * @param clientName The name of the client.
+     * @return The address of the client.
+     */
+    public String getClientAdressByName(String clientName) {
         return discoveryClient.getInstances(clientName).get(0).getUri().toString();
     }
 
-    public boolean existEurekaDiscoveryClientByName(String name){
-        return discoveryClient.getServices().stream().filter(microserviceName-> microserviceName.equals(name)).count()==1;
+    /**
+     * Checks if a Eureka discovery client exists by name.
+     *
+     * @param name The name of the client to check.
+     * @return true if the client exists, false otherwise.
+     */
+    public boolean existEurekaDiscoveryClientByName(String name) {
+        return discoveryClient.getServices().stream().filter(microserviceName -> microserviceName.equals(name)).count() == 1;
     }
 
 }
