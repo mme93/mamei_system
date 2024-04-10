ad9ba6eedc8dc194fad85bc0a8c92ab6ac7d4e5d
magantus@googlemail.com <Extrem93!>
2024-04-10 22:07:50 +0200

    update

---
+++ backend/systemmanager/src/test/java/com/systemmanager/process/story/ProcessStoryTest.java
@@ -0,0 +1,17 @@
+package com.systemmanager.process.story;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the hole Process Service.
+ */
+public class ProcessStoryTest {
+
+
+    @Test
+    void test(){
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+
+}
