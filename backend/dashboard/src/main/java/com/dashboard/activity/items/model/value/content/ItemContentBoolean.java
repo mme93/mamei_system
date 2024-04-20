b5d17183978e1c03c0db4fd2adde521e53309459
magantus@googlemail.com <Extrem93!>
2024-04-20 20:39:09 +0200

    update

---
+++ backend/dashboard/src/main/java/com/dashboard/activity/items/model/value/content/ItemContentBoolean.java
@@ -0,0 +1,26 @@
+package com.dashboard.activity.items.model.value.content;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "item_value_content_boolean")
+public class ItemContentBoolean {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private Boolean check;
+
+    private String contentName;
+
+}
