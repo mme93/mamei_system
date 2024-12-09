b521824478c7caaaf4124dc99d74482019f42569
magantus@googlemail.com <Extrem93!>
2024-12-09 20:09:26 +0100

    update

--- frontends/game-frontend/src/app/pixelquest/world/world.component.ts
+++ frontends/game-frontend/src/app/pixelquest/world/world.component.ts
@@ -9,6 +9,11 @@ import { ScreenSizeService } from '../../service/screen/screen-size.service';
 })
 export class WorldComponent implements OnInit, OnDestroy {
 
+  example:String[][]=[];
+  blockWidth:number=0;
+  blockHight:number=0;
+  rows:number=14;
+  cols:number=32;
   screenSize: { width: number, height: number } | null = null;
   private subscription!: Subscription;
 
@@ -20,7 +25,21 @@ export class WorldComponent implements OnInit, OnDestroy {
         width: (size.width * 0.7),
         height: (size.height * 0.7)
       };
+      this.blockHight=(size.height * 0.7)/this.rows;
+      this.blockWidth=(size.width * 0.7)/this.cols;
     });
+    this.createGrid();
+  }
+
+  createGrid(){
+    for(let i=0;i<this.rows;i++){
+      let x:String[]=[];
+      for(let j=0;j<this.cols;j++){
+        x.push(String(((i*10)+j+1)))
+      }
+      this.example.push(x);
+    }
+  
   }
 
   ngOnDestroy(): void {
