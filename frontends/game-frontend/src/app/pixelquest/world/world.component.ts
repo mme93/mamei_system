901d97c8620333365b53548662fe00f548af37c1
magantus@googlemail.com <Extrem93!>
2024-12-09 10:54:44 +0100

    update

--- frontends/game-frontend/src/app/pixelquest/world/world.component.ts
+++ frontends/game-frontend/src/app/pixelquest/world/world.component.ts
@@ -6,9 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./world.component.scss']
 })
 export class WorldComponent implements OnInit {
-  
+
+  test: String[][] = [];
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.createGridElements();
   }
-  
+
+  createGridElements() {
+    const row = 50;
+    const col = 50;
+    for (var i: number = 0; i < row; i++) {
+      let rowArray: String[] = [];
+      for (var j: number = 0; j < col; j++){
+        rowArray.push(String((i * 10) + 1 + j));
+      }
+      this.test.push(rowArray);
+    }
+  }
 }
