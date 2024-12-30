9e2aeeed82dc8a0ab81969b6ebe3e4e8eaf8eb05
magantus@googlemail.com <Extrem93!>
2024-12-31 00:06:34 +0100

    update

--- frontends/mameie-frontend/src/app/canvas/canvas.component.ts
+++ frontends/mameie-frontend/src/app/canvas/canvas.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CanvasContentComponent } from "./canvas-content/canvas-content.component";
+import { CanvasDeviceComponent } from "./canvas-device/canvas-device.component";
 
 @Component({
   selector: 'app-canvas',
   standalone: true,
-  imports: [CanvasContentComponent],
+  imports: [CanvasContentComponent, CanvasDeviceComponent],
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.scss'
 })
