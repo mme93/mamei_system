a0361b30f004bf963f7b1eceb1d9c7d842baa85b
magantus@googlemail.com <Extrem93!>
2025-01-12 23:20:32 +0100

    update

--- frontends/frontend/src/app/pages/dashboard/ticket-overview/ticket-overview.module.ts
+++ frontends/frontend/src/app/pages/dashboard/ticket-overview/ticket-overview.module.ts
@@ -14,6 +14,7 @@ import { MatDatepickerModule } from "@angular/material/datepicker";
 import { MatSelectModule } from "@angular/material/select";
 import { TicketComponent } from "./ticket/ticket.component";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 
 
@@ -32,7 +33,8 @@ import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
         MatNativeDateModule,
         MatButtonModule,
         MatIconModule,
-        MatProgressSpinnerModule
+        MatProgressSpinnerModule,
+        MatProgressBarModule
     ],
     exports: [],
     declarations: [
