b8fd27c5f83814b83f7b3a6b1ef9d917d17dafb5
magantus@googlemail.com <Extrem93!>
2025-01-20 00:25:59 +0100

    add method to delete and update filter

--- frontends/frontend/src/app/shared/services/dashboard/ticket/ticket-table-filter.service.ts
+++ frontends/frontend/src/app/shared/services/dashboard/ticket/ticket-table-filter.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { TicketTableFilter } from 'src/app/shared/model/settings/TicketSettings';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketTableFilterService {
 
-  constructor() { }
+  private createTicketUrl = environment.uri + ':9052/ticket/table/settings/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  updateFilter(filter: TicketTableFilter) {
+    return this.http.put<TicketTableFilter>(this.createTicketUrl, filter);
+  }
+
+  deleteFilter(filterName: string) {
+    return this.http.delete(this.createTicketUrl + filterName);
+  }
+
 }
