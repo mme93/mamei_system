55ea446956ff1ebc26b3968b2ccdb2c26d525cd7
magantus@googlemail.com <Extrem93!>
2025-01-19 15:10:11 +0100

    remove old method and add new method to set label for mat-menu and save the change of the status

--- frontends/frontend/src/app/pages/dashboard/ticket-overview/ticket/ticket.component.ts
+++ frontends/frontend/src/app/pages/dashboard/ticket-overview/ticket/ticket.component.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Ticket } from 'src/app/shared/model/dashboard/Ticket';
+import { Ticket, TicketStatusDropDown } from 'src/app/shared/model/dashboard/Ticket';
 import { TicketService } from 'src/app/shared/services/dashboard/ticket/ticket.service';
 
 @Component({
@@ -10,9 +10,10 @@ import { TicketService } from 'src/app/shared/services/dashboard/ticket/ticket.s
   styleUrl: './ticket.component.scss'
 })
 export class TicketComponent implements OnInit {
+  statusLabels: TicketStatusDropDown[] = [];
   loading: boolean = true;
   ticket: Ticket | null = null;
-  constructor(private route: ActivatedRoute, private ticketService: TicketService,private router:Router) { }
+  constructor(private route: ActivatedRoute, private ticketService: TicketService, private router: Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
@@ -20,6 +21,7 @@ export class TicketComponent implements OnInit {
       this.ticketService.getTicketById(Number(params['id'])).subscribe(result => {
         this.ticket = result;
         this.loading = false;
+        this.setLabelText(result);
       },
         (error: HttpErrorResponse) => {
           console.log('HTTP Status:', error.status);
@@ -28,11 +30,92 @@ export class TicketComponent implements OnInit {
     });
   }
 
-  backToOverview(){
+  backToOverview() {
     this.router.navigate(['/dashboard/ticket/overview']);
   }
-  onStatusChange(ticket: Ticket): void {
-    console.log(`Status updated to: ${ticket.status}`);
+
+  changeStatus(status: string) {
+    if (this.ticket && this.ticket.id) {
+      this.ticketService.changeTicketStatus(status, this.ticket.id).subscribe(result => {
+        this.ticket = result;
+        this.setLabelText(result);
+      });
+    }
+  }
+
+  setLabelText(ticket: Ticket) {
+    this.statusLabels = [];
+    if (ticket.status === 'CREATED') {
+      this.statusLabels.push({
+        label: 'Set to In Progress',
+        code: 'IN_PROGRESS'
+      });
+      this.statusLabels.push({
+        label: 'Set to Waiting',
+        code: 'WAITING'
+      });
+      this.statusLabels.push({
+        label: 'Set to Refinement',
+        code: 'REFINEMENT'
+      });
+      this.statusLabels.push({
+        label: 'Set to Done',
+        code: 'DONE'
+      });
+    } else if (ticket.status === 'WAITING') {
+      this.statusLabels.push({
+        label: 'Set to Refinement',
+        code: 'REFINEMENT'
+      });
+      this.statusLabels.push({
+        label: 'Set to In Progress',
+        code: 'IN_PROGRESS'
+      });
+      this.statusLabels.push({
+        label: 'Set to Done',
+        code: 'DONE'
+      });
+    } else if (ticket.status === 'REFINEMENT') {
+      this.statusLabels.push({
+        label: 'Set to Waiting',
+        code: 'WAITING'
+      });
+      this.statusLabels.push({
+        label: 'Set to In Progress',
+        code: 'IN_PROGRESS'
+      });
+      this.statusLabels.push({
+        label: 'Set to Done',
+        code: 'DONE'
+      });
+    } else if (ticket.status === 'IN_PROGRESS') {
+      this.statusLabels.push({
+        label: 'Set to Waiting',
+        code: 'WAITING'
+      });
+      this.statusLabels.push({
+        label: 'Set to Refinement',
+        code: 'REFINEMENT'
+      });
+      this.statusLabels.push({
+        label: 'Set to Done',
+        code: 'DONE'
+      });
+    } else if (ticket.status === 'DONE') {
+      this.statusLabels.push({
+        label: 'Set to Waiting',
+        code: 'WAITING'
+      });
+      this.statusLabels.push({
+        label: 'Set to Refinement',
+        code: 'REFINEMENT'
+      });
+      this.statusLabels.push({
+        label: 'Set to In Progress',
+        code: 'IN_PROGRESS'
+      });
+    }
+
   }
 
 }
