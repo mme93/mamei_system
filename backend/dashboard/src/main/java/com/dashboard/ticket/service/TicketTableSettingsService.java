4562b7bd5dacdc08114fd40e9e1049b437c97d49
magantus@googlemail.com <Extrem93!>
2025-01-20 00:26:29 +0100

    create service and add method to delete filter

---
+++ backend/dashboard/src/main/java/com/dashboard/ticket/service/TicketTableSettingsService.java
@@ -0,0 +1,11 @@
+package com.dashboard.ticket.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketTableSettingsService {
+
+    public void deleteFilterByName(String filterName) {
+
+    }
+}
