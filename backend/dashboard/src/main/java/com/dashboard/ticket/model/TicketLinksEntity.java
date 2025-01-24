cee58190a523712ebcebac8c7dff0e843c3a6947
magantus@googlemail.com <Extrem93!>
2025-01-24 21:36:22 +0100

    init TicketLinksEntity

---
+++ backend/dashboard/src/main/java/com/dashboard/ticket/model/TicketLinksEntity.java
@@ -0,0 +1,31 @@
+package com.dashboard.ticket.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket_links")
+public class TicketLinksEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private Long ticketId;
+
+    @Column(length = 2000)
+    private String linkedTicketIds;
+
+    public TicketLinksEntity(Long ticketId, String linkedTicketIds) {
+        this.ticketId = ticketId;
+        this.linkedTicketIds = linkedTicketIds;
+    }
+}
