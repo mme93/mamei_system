030f848cf75dab4ff4aa066c537a3cd4cec608cd
magantus@googlemail.com <Extrem93!>
2025-01-25 00:11:25 +0100

    add crud service method

--- backend/dashboard/src/main/java/com/dashboard/ticket/service/TicketFilterService.java
+++ backend/dashboard/src/main/java/com/dashboard/ticket/service/TicketFilterService.java
@@ -1,10 +1,16 @@
 package com.dashboard.ticket.service;
 
+import com.dashboard.ticket.model.dto.TicketTableFilterCreateDto;
+import com.dashboard.ticket.model.dto.TicketTableFilterResponseDto;
 import com.dashboard.ticket.model.entity.TicketTableFilterEntity;
+import com.dashboard.ticket.model.mapper.TicketFilterMapper;
 import com.dashboard.ticket.repository.TicketTableFilterRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TicketFilterService {
@@ -15,11 +21,37 @@ public class TicketFilterService {
         this.ticketTableFilterRepository = ticketTableFilterRepository;
     }
 
-    public List<TicketTableFilterEntity> getAllTicketTableFilter() {
-        return ticketTableFilterRepository.findAll();
+    public List<TicketTableFilterResponseDto> getAllTicketTableFilter() throws JsonProcessingException {
+        List<TicketTableFilterResponseDto>tableFilterResponseDtos=new ArrayList<>();
+        for(TicketTableFilterEntity entity:ticketTableFilterRepository.findAll()){
+            tableFilterResponseDtos.add(TicketFilterMapper.entityMapToDto(entity));
+        }
+        return tableFilterResponseDtos;
     }
 
-    public TicketTableFilterEntity createTicketTableFilter() {
-        return null;
+    public TicketTableFilterEntity createTicketTableFilter(TicketTableFilterCreateDto filterCreateDto) {
+        try {
+            TicketTableFilterEntity entity = TicketFilterMapper.dtoMapToEntity(filterCreateDto);
+            return ticketTableFilterRepository.save(entity);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public TicketTableFilterEntity updateTicketTableFilter(TicketTableFilterResponseDto filterCreateDto) {
+        try {
+            TicketTableFilterEntity entity = TicketFilterMapper.dtoMapToEntity(filterCreateDto);
+            return ticketTableFilterRepository.save(entity);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<TicketTableFilterEntity> getTicketTableFilterById(Long id) {
+        return ticketTableFilterRepository.findById(id);
+    }
+
+    public void deleteTicketTableFilterById(Long id) {
+        ticketTableFilterRepository.deleteById(id);
     }
 }
