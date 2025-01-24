22adac4deb274729153842d389ee3879e6df63e4
magantus@googlemail.com <Extrem93!>
2025-01-25 00:12:32 +0100

    create dto for  TicketTableFilter

--- backend/dashboard/src/main/java/com/dashboard/ticket/model/dto/TicketTableFilterResponseDto.java
+++ backend/dashboard/src/main/java/com/dashboard/ticket/model/dto/TicketTableFilterResponseDto.java
@@ -4,8 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class TicketTableFilterResponseDto {
+
+    private String filterName;
+
+    private boolean isCREATED;
+
+    private boolean isWAITING;
+
+    private boolean isREFINEMENT;
+
+    private boolean isIN_PROGRESS;
+
+    private boolean isDONE;
+
+    private List<String> displayedColumns;
 }
