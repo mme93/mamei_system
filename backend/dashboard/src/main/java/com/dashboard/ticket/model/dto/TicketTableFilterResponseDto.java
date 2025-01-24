c3fc6775ef7bb3f4a224f022a56595ac0c8e6953
magantus@googlemail.com <Extrem93!>
2025-01-24 22:04:27 +0100

    create TicketTableFilterResponseDto for existing filter

---
+++ backend/dashboard/src/main/java/com/dashboard/ticket/model/dto/TicketTableFilterResponseDto.java
@@ -0,0 +1,11 @@
+package com.dashboard.ticket.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TicketTableFilterResponseDto {
+}
