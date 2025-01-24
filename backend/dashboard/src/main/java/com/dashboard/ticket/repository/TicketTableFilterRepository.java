6a67a153e7da9472ec84ca9ebfc0b02787f2d409
magantus@googlemail.com <Extrem93!>
2025-01-24 21:57:44 +0100

    create TicketTableFilterEntity Repository

---
+++ backend/dashboard/src/main/java/com/dashboard/ticket/repository/TicketTableFilterRepository.java
@@ -0,0 +1,7 @@
+package com.dashboard.ticket.repository;
+
+import com.dashboard.ticket.model.TicketTableFilterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketTableFilterRepository extends JpaRepository<TicketTableFilterEntity, Long> {
+}
