ebcd5ba02d0fe87ca9a9103634426a1452ebdf59
magantus@googlemail.com <Extrem93!>
2025-01-24 21:57:37 +0100

    create TicketTableFilterEntity

---
+++ backend/dashboard/src/main/java/com/dashboard/ticket/model/TicketTableFilterEntity.java
@@ -0,0 +1,21 @@
+package com.dashboard.ticket.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket_table_filter")
+public class TicketTableFilterEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+}
