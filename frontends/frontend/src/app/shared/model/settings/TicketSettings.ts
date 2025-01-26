f55ac19eb9ac65659d928eb96ff5f585d872ade9
magantus@googlemail.com <Extrem93!>
2025-01-27 00:15:47 +0100

    add test interface

--- frontends/frontend/src/app/shared/model/settings/TicketSettings.ts
+++ frontends/frontend/src/app/shared/model/settings/TicketSettings.ts
@@ -1,19 +1,29 @@
 export interface TicketTableSettings {
     selectedFilter: TicketTableFilter;
     filter: TicketTableFilter[];
-    defaultCoulmns:string[];
+    defaultCoulmns: string[];
 }
 
-export interface TicketTableStatusFilter{
-    isCREATED:boolean;
-    isWAITING:boolean;
-    isREFINEMENT:boolean;
-    isIN_PROGRESS:boolean;
-    isDONE:boolean;
+export interface TicketTableStatusFilter {
+    isCREATED: boolean;
+    isWAITING: boolean;
+    isREFINEMENT: boolean;
+    isIN_PROGRESS: boolean;
+    isDONE: boolean;
 }
 
 export interface TicketTableFilter {
     name: string;
     statusFilter: TicketTableStatusFilter;
-    displayedColumns:string[];
+    displayedColumns: string[];
+}
+
+export interface X {
+    filterName: string;
+    done: boolean;
+    create: boolean;
+    in_PROGRESS: boolean;
+    refinement: boolean;
+    waiting: boolean;
+    displayedColumns: string[];
 }
