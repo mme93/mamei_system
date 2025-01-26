6d146e00d49bac301fc04f97e877d7a5a6795133
magantus@googlemail.com <Extrem93!>
2025-01-27 00:15:36 +0100

    new http request for specific filter and all filters

--- frontends/frontend/src/app/shared/services/dashboard/ticket/ticket-table-filter.service.ts
+++ frontends/frontend/src/app/shared/services/dashboard/ticket/ticket-table-filter.service.ts
@@ -1,14 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TicketTableFilter } from 'src/app/shared/model/settings/TicketSettings';
+import { TicketTableFilter, X } from 'src/app/shared/model/settings/TicketSettings';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketTableFilterService {
+  
 
   private createTicketUrl = environment.uri + ':9052/ticket/table/settings/';
+  private ticketFilterUrl = environment.uri + ':9052/ticket/filter';
 
   constructor(private http: HttpClient) {
   }
@@ -19,6 +21,14 @@ export class TicketTableFilterService {
 
   deleteFilter(filterName: string) {
     return this.http.delete(this.createTicketUrl + filterName);
+  }
+
+  getFilterById(id:number){
+    return this.http.get<X>(this.ticketFilterUrl+"/table/"+id);
+  }
+
+  getFilters() {
+    return this.http.get<X[]>(this.ticketFilterUrl+"/table");
   }
 
 }
