7fab7a48e2649ad86c1daf29265911473509373e
magantus@googlemail.com <Extrem93!>
2025-01-30 22:26:41 +0100

    create FrontendFilterConfig with dummy value

---
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/core/filter/model/FrontendFilterConfig.java
@@ -0,0 +1,16 @@
+package com.configmanager.settings.frontend.core.filter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class FrontendFilterConfig {
+
+    String test;
+
+}
