c833eade5ed7e9f7816974fa764d4b9ff74cb2d2
magantus@googlemail.com <Extrem93!>
2025-01-30 14:08:30 +0100

    change used interfaces

--- frontends/frontend/src/app/pages/dashboard/ticket-overview/ticket-settings-dialog/ticket-settings-dialog.component.ts
+++ frontends/frontend/src/app/pages/dashboard/ticket-overview/ticket-settings-dialog/ticket-settings-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { TicketTableFilter, TicketTableSettings, X } from 'src/app/shared/model/settings/TicketSettings';
+import { TicketTableFilter } from 'src/app/shared/model/settings/TicketSettings';
 import { TicketTableFilterService } from 'src/app/shared/services/dashboard/ticket/ticket-table-filter.service';
 
 @Component({
@@ -8,51 +8,33 @@ import { TicketTableFilterService } from 'src/app/shared/services/dashboard/tick
   templateUrl: './ticket-settings-dialog.component.html',
   styleUrls: ['./ticket-settings-dialog.component.scss']
 })
-export class TicketSettingsDialogComponent implements OnInit{
-  currentCopy: TicketTableFilter | undefined;
+export class TicketSettingsDialogComponent implements OnInit {
+  newTicketFilterName = '';
   isEditable = false;
   isNewFilter = false;
-  settings: TicketTableSettings;
-  filterNames: string[] = [];
-  filterName = '';
-  defaultColumns: string[] = [];
-  newTicketFilterName = '';
-  selectedFilter: TicketTableFilter = {
-    name: '',
-    statusFilter: {
-      isCREATED: false,
-      isWAITING: false,
-      isREFINEMENT: false,
-      isIN_PROGRESS: false,
-      isDONE: false
-    },
-    displayedColumns: []
-  };
   isALL = false;
-
-  filters: X[] = [];
-  selectedFilters: X = {
+  filters: TicketTableFilter[] = [];
+  selectedFilter: TicketTableFilter = {
     filterName: '',
     done: true,
-    create: true,
+    created: true,
     in_PROGRESS: true,
     refinement: true,
     waiting: true,
     displayedColumns: []
   };
+  copyFilter = this.selectedFilter;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: TicketTableSettings, private filterService: TicketTableFilterService,
+    @Inject(MAT_DIALOG_DATA) public data: TicketTableFilter, private filterService: TicketTableFilterService,
     private dialogRef: MatDialogRef<TicketSettingsDialogComponent>
   ) {
-    this.settings = { ...data };
-    this.settings.filter.forEach(filter => this.filterNames.push(filter.name));
-    const name = this.settings.selectedFilter.name;
-    this.filterName = name;
-    this.changeFilter(name);
+    this.selectedFilter = data;
   }
+
+
   ngOnInit(): void {
-   this.filterService.getFilters().subscribe((result:X[])=>this.filters=result);
+    this.filterService.getFilters().subscribe((result: TicketTableFilter[]) => this.filters = result);
   }
 
   onCancle() {
@@ -60,63 +42,56 @@ export class TicketSettingsDialogComponent implements OnInit{
   }
 
   onClose(): void {
-    this.settings.selectedFilter = this.selectedFilter;
-    this.dialogRef.close(this.settings);
+    this.dialogRef.close(this.selectedFilter);
   }
 
   resetDefault(): void {
-    if (this.filterName === 'default') {
-      this.selectedFilter = {
-        name: 'default',
-        statusFilter: {
-          isCREATED: true,
-          isWAITING: true,
-          isREFINEMENT: true,
-          isIN_PROGRESS: true,
-          isDONE: true
-        },
-        displayedColumns: this.settings.defaultCoulmns
-      }
-    }
+
   }
 
   changeFilter(filterName: string): void {
-    this.settings.filter.forEach(filter => {
-      if (filterName === filter.name) {
+    this.filters.forEach(filter => {
+      if (filterName === filter.filterName) {
+        console.log(filter)
         this.selectedFilter.displayedColumns = [...filter.displayedColumns];
-        this.selectedFilter.name = filter.name;
-        this.selectedFilter.statusFilter = { ...filter.statusFilter };
-        this.defaultColumns = [...filter.displayedColumns];
-
-        this.currentCopy = {
-          displayedColumns: [...filter.displayedColumns],
-          name: filter.name,
-          statusFilter: { ...filter.statusFilter }
-        }
+        this.selectedFilter.filterName = filter.filterName;
+        this.selectedFilter.created = filter.created;
+        this.selectedFilter.in_PROGRESS = filter.in_PROGRESS;
+        this.selectedFilter.refinement = filter.refinement;
+        this.selectedFilter.waiting = filter.waiting;
+        this.selectedFilter.done = filter.done;
+        console.log(this.selectedFilter)
+        console.log(filter.created)
+        this.copyFilter.displayedColumns = [...filter.displayedColumns];
+        this.copyFilter.filterName = filter.filterName;
+        this.copyFilter.created = filter.created;
+        this.copyFilter.in_PROGRESS = filter.in_PROGRESS;
+        this.copyFilter.refinement = filter.refinement;
+        this.copyFilter.waiting = filter.waiting;
+        this.copyFilter.done = filter.done;
 
       }
     });
-    this.isEditable = this.filterName === 'default';
+    this.isEditable = this.selectedFilter.filterName === 'default';
     this.changeCheckbox();
   }
   changeIsAllCheckbox(): void {
-    this.selectedFilter.statusFilter.isCREATED = this.isALL;
-    this.selectedFilter.statusFilter.isWAITING = this.isALL;
-    this.selectedFilter.statusFilter.isREFINEMENT = this.isALL;
-    this.selectedFilter.statusFilter.isIN_PROGRESS = this.isALL;
-    this.selectedFilter.statusFilter.isDONE = this.isALL;
+    this.selectedFilter.created = this.isALL;
+    this.selectedFilter.waiting = this.isALL;
+    this.selectedFilter.refinement = this.isALL;
+    this.selectedFilter.in_PROGRESS = this.isALL;
+    this.selectedFilter.done = this.isALL;
   }
   changeCheckbox(): void {
-    this.isALL = this.selectedFilter.statusFilter.isCREATED &&
-      this.selectedFilter.statusFilter.isWAITING &&
-      this.selectedFilter.statusFilter.isREFINEMENT &&
-      this.selectedFilter.statusFilter.isIN_PROGRESS &&
-      this.selectedFilter.statusFilter.isDONE;
+    this.isALL = this.selectedFilter.created &&
+      this.selectedFilter.waiting &&
+      this.selectedFilter.refinement &&
+      this.selectedFilter.in_PROGRESS &&
+      this.selectedFilter.done;
   }
 
   resetRows() {
-    this.defaultColumns = this.settings.defaultCoulmns;
-    this.selectedFilter.displayedColumns = this.settings.defaultCoulmns;
+    this.selectedFilter.displayedColumns = this.selectedFilter.displayedColumns;
   }
   newFilter() {
     this.isNewFilter = !this.isNewFilter;
@@ -128,9 +103,13 @@ export class TicketSettingsDialogComponent implements OnInit{
 
   createNewFilter() {
     this.filterService.updateFilter({
-      name: this.newTicketFilterName,
-      statusFilter: this.selectedFilter.statusFilter,
-      displayedColumns: this.selectedFilter.displayedColumns
+      filterName: this.newTicketFilterName,
+      done: true,
+      created: true,
+      in_PROGRESS: true,
+      refinement: true,
+      waiting: true,
+      displayedColumns: []
     } as TicketTableFilter);
   }
 
@@ -140,7 +119,7 @@ export class TicketSettingsDialogComponent implements OnInit{
   }
 
   deleteFilter() {
-    this.filterService.deleteFilter(this.filterName);
+    this.filterService.deleteFilter(this.selectedFilter.filterName);
   }
 
 }
