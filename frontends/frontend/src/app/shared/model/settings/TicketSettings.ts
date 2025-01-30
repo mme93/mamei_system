56a93aea970182e4d7da9e4895aba0665909cadd
magantus@googlemail.com <Extrem93!>
2025-01-30 14:07:38 +0100

    remove old/unused interfaces

--- frontends/frontend/src/app/shared/model/settings/TicketSettings.ts
+++ frontends/frontend/src/app/shared/model/settings/TicketSettings.ts
@@ -1,27 +1,7 @@
-export interface TicketTableSettings {
-    selectedFilter: TicketTableFilter;
-    filter: TicketTableFilter[];
-    defaultCoulmns: string[];
-}
-
-export interface TicketTableStatusFilter {
-    isCREATED: boolean;
-    isWAITING: boolean;
-    isREFINEMENT: boolean;
-    isIN_PROGRESS: boolean;
-    isDONE: boolean;
-}
-
 export interface TicketTableFilter {
-    name: string;
-    statusFilter: TicketTableStatusFilter;
-    displayedColumns: string[];
-}
-
-export interface X {
     filterName: string;
     done: boolean;
-    create: boolean;
+    created: boolean;
     in_PROGRESS: boolean;
     refinement: boolean;
     waiting: boolean;
