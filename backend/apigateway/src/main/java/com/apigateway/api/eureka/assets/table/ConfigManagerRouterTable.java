5ef57a7b8c26d1af1095f36036b13a35c07bcd84
magantus@googlemail.com <Extrem93!>
2025-01-30 22:25:45 +0100

    create ConfigManagerRouterTable with ping rout

---
+++ backend/apigateway/src/main/java/com/apigateway/api/eureka/assets/table/ConfigManagerRouterTable.java
@@ -0,0 +1,9 @@
+package com.apigateway.api.eureka.assets.table;
+
+public class ConfigManagerRouterTable {
+
+    public static final String CONFIG_MANAGER_ID = "CONFIGMANAGER";
+
+    public static final String CONFIG_MANAGER_PING = "/configmanager/ping";
+
+}
