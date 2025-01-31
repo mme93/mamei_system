176647101f80d3d1f60bda3b88e35b6d17236b3f
magantus@googlemail.com <Extrem93!>
2025-02-01 00:26:19 +0100

    add ticket routes

--- backend/apigateway/src/main/java/com/apigateway/api/eureka/assets/table/DashboardRouterTable.java
+++ backend/apigateway/src/main/java/com/apigateway/api/eureka/assets/table/DashboardRouterTable.java
@@ -1,5 +1,7 @@
 package com.apigateway.api.eureka.assets.table;
 
+import java.util.List;
+
 /**
  * Router table for Dashboard service.
  */
@@ -15,4 +17,30 @@ public class DashboardRouterTable {
 
     /** URI for getting all tasks. */
     public static final String URI_DASH_BOARD_GET_ALL_TASK = "/task/all";
+
+    /** ----------- TICKET ----------- */
+
+    public static final String URI_DASH_BOARD_CREATE_TICKET= "/ticket/";
+
+    public static final String URI_DASH_BOARD_GET_TICKET_BY_ID= "/ticket/";
+
+    public static final String URI_DASH_BOARD_GET_ALL_TICKETS= "/ticket/";
+
+    public static final String URI_DASH_BOARD_DELETE_TICKET_BY_ID= "/ticket/";
+
+    public static final String URI_DASH_BOARD_UPDATE_TICKET_STATUS= "/ticket/status/";
+
+    public static final String URI_DASH_BOARD_UPDATE_TICKET= "/ticket/";
+
+    public static final String URI_DASH_BOARD_GET_ALL_TICKET_TABLE_FILTER= "/ticket/filter/table";
+
+    public static final String URI_DASH_BOARD_GET_TICKET_TABLE_FILTER_BY_ID= "/ticket/filter/table/";
+
+    public static final String URI_DASH_BOARD_CREATE_TICKET_TABLE_FILTER= "/ticket/filter/table";
+
+    public static final String URI_DASH_BOARD_UPDATE_TICKET_TABLE_FILTER= "/ticket/filter/table";
+
+    public static final String URI_DASH_BOARD_DELETE_TICKET_TABLE_FILTER_BY_ID= "/ticket/filter/table/";
+
+
 }
