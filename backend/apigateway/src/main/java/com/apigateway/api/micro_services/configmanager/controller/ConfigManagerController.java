b6257f36a6bd645c0ca337d85021fb359c043386
magantus@googlemail.com <Extrem93!>
2025-02-01 00:25:28 +0100

    add method to get config for specific frontendName

--- backend/apigateway/src/main/java/com/apigateway/api/micro_services/configmanager/controller/ConfigManagerController.java
+++ backend/apigateway/src/main/java/com/apigateway/api/micro_services/configmanager/controller/ConfigManagerController.java
@@ -1,9 +1,11 @@
 package com.apigateway.api.micro_services.configmanager.controller;
 
 import com.apigateway.api.micro_services.configmanager.service.ConfigManagerService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,12 @@ public class ConfigManagerController {
     @GetMapping("/ping")
     public ResponseEntity<String> getPing(){
         return new ResponseEntity<>( configManagerService.ping(), HttpStatus.OK);
+    }
+
+    @GetMapping("/frontend/config/{frontendName}/filter")
+    public ResponseEntity<Object> getFilterConfig(HttpServletRequest request, @PathVariable String frontendName){
+        String username = (String) request.getAttribute("X-Username");
+        return new ResponseEntity<>( configManagerService.getFilterConfig(username,frontendName), HttpStatus.OK);
     }
 
 }
