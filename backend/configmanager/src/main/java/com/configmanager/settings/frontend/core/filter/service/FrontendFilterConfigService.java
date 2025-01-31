a90cc4ba6eb4fb0385a237a8e00b0d6fd7d6bd23
magantus@googlemail.com <Extrem93!>
2025-02-01 00:29:01 +0100

    add service method for filterconfig

--- backend/configmanager/src/main/java/com/configmanager/settings/frontend/core/filter/service/FrontendFilterConfigService.java
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/core/filter/service/FrontendFilterConfigService.java
@@ -1,7 +1,14 @@
 package com.configmanager.settings.frontend.core.filter.service;
 
+import com.configmanager.settings.frontend.core.filter.model.FrontendFilterConfig;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FrontendFilterConfigService {
+
+    public FrontendFilterConfig getFilterConfig(String frontendName, String username) {
+
+
+        return new FrontendFilterConfig("DAs ist ein ConfigFilter");
+    }
 }
