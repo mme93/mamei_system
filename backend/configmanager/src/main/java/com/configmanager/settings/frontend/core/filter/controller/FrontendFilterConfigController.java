0d1901f9a68ef65858c5d9b29b14b69c6609cc4f
magantus@googlemail.com <Extrem93!>
2025-02-01 00:28:53 +0100

    add get method for filterconfig

--- backend/configmanager/src/main/java/com/configmanager/settings/frontend/core/filter/controller/FrontendFilterConfigController.java
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/core/filter/controller/FrontendFilterConfigController.java
@@ -1,11 +1,10 @@
 package com.configmanager.settings.frontend.core.filter.controller;
 
+import com.configmanager.settings.frontend.core.filter.model.FrontendFilterConfig;
 import com.configmanager.settings.frontend.core.filter.service.FrontendFilterConfigService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/frontend/config/{frontendName}/filter")
@@ -18,16 +17,8 @@ public class FrontendFilterConfigController {
     }
 
     @GetMapping("/")
-    public ResponseEntity<String> getFilters() {
-
-        return null;
-    }
-
-    @GetMapping("/{filterName}")
-    public ResponseEntity<String> getFilter(@PathVariable String filterName, @PathVariable String frontendName) {
-        System.err.println(frontendName);
-        System.err.println(filterName);
-        return null;
+    public ResponseEntity<FrontendFilterConfig> getFilterConfig(@RequestHeader(value = "X-Username", required = false) String username, @PathVariable String frontendName) {
+        return new ResponseEntity<>(filterConfigService.getFilterConfig(frontendName,username), HttpStatus.OK);
     }
 
 
