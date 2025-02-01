a6134877e95e4bcb9eedfee1feb9f622ecd26726
magantus@googlemail.com <Extrem93!>
2025-02-01 23:54:19 +0100

    mamei frontend service for ticketTable

---
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/tickettable/MaMeiTicketTableService.java
@@ -0,0 +1,19 @@
+package com.configmanager.settings.frontend.projects.mamei.tickettable;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MaMeiTicketTableService {
+
+    public MaMeiTicketTableConfigDto loadConfig(String username) {
+
+        return null;
+    }
+
+    public Object
+
+    createConfig(Object config) {
+
+        return null;
+    }
+}
