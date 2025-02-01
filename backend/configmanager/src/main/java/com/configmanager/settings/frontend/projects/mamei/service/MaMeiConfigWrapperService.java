f6a5514af1904c913ec43dd1323384909ac43862
magantus@googlemail.com <Extrem93!>
2025-02-02 00:53:05 +0100

    add cast method

--- backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/service/MaMeiConfigWrapperService.java
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/service/MaMeiConfigWrapperService.java
@@ -1,13 +1,18 @@
 package com.configmanager.settings.frontend.projects.mamei.service;
 
-import com.configmanager.settings.frontend.projects.mamei.tickettable.MaMeiTicketTableConfigDto;
-import com.configmanager.settings.frontend.projects.mamei.tickettable.MaMeiTicketTableConfigEntity;
+import com.configmanager.settings.frontend.projects.mamei.model.dto.MaMeiTicketTableConfigDto;
+import com.configmanager.settings.frontend.projects.mamei.model.entity.MaMeiTicketTableConfigEntity;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MaMeiConfigWrapperService {
 
-    public MaMeiTicketTableConfigEntity wrapToTicketTableConfigEntity(Object object) {
+    public MaMeiTicketTableConfigEntity wrapToTicketTableConfigEntity(Object object) throws JsonProcessingException {
         MaMeiTicketTableConfigDto dto = (MaMeiTicketTableConfigDto) object;
         return wrapToTicketTableConfigEntity(dto);
     }
@@ -17,8 +22,25 @@ public class MaMeiConfigWrapperService {
                 dto.getOwner(),
                 dto.getCurrentFilterName(),
                 dto.getCurrentTableId(),
-                dto.getTableFilterIds()
+                listToString(dto.getTableFilterIds().stream().map(object -> object.toString()).toList())
         );
+    }
+
+    private static List<String> stringToList(String value) {
+        try {
+            return new ObjectMapper().readValue(value, ArrayList.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String listToString(List<String> list) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(list);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
