d7dea6d9ddbfb73e2fea4e1e6293169e68fdb229
magantus@googlemail.com <Extrem93!>
2025-02-01 23:52:48 +0100

    basic service for frontend configs

---
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/core/service/FrontendConfigService.java
@@ -0,0 +1,12 @@
+package com.configmanager.settings.frontend.core.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FrontendConfigService {
+
+    public Object getFrontendConfig(String username, String frontendName) {
+
+        return null;
+    }
+}
