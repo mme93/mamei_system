3b265cdca9637faca89e759d48af00eb717ec13a
magantus@googlemail.com <Extrem93!>
2025-02-08 00:45:07 +0100

    data for the different dropdown menus

---
+++ frontends/frontend/src/app/shared/data/TicketData.ts
@@ -0,0 +1,45 @@
+export const ticketDropDown = {
+    systems: [
+        { value: 'BACKEND', viewValue: 'Backend' },
+        { value: 'FRONTEND', viewValue: 'Frontend' },
+    ],
+    types: [
+        { value: 'TASK', viewValue: 'Task' },
+        { value: 'BUG', viewValue: 'Bug' },
+        { value: 'FEATURE', viewValue: 'Feature' },
+        { value: 'IMPEDIMENT', viewValue: 'Impediment' }
+    ],
+    labels: [
+        { value: 'CODE', viewValue: 'Code' },
+        { value: 'CODE_QUALITY', viewValue: 'Code Quality' },
+        { value: 'CODE_CLEARING', viewValue: 'Code clearing' },
+        { value: 'CONFIGURATION', viewValue: 'Configuration' },
+        { value: 'API_ROUTING', viewValue: 'Api Routing' },
+        { value: 'TEST', viewValue: 'Test' }
+    ],
+    classifications: [
+        { value: 'HIGH', viewValue: 'High' },
+        { value: 'MIDDLE', viewValue: 'Middle' },
+        { value: 'LOW', viewValue: 'Low' }
+    ],
+    projects_frontend: [
+        { value: 'NOTHING_SELECTED', viewValue: 'Nothing selected' },
+        { value: 'MAMEI_SYSTEM', viewValue: 'MaMei System' },
+        { value: 'DB_MANAGER', viewValue: 'DB Manager' }
+    ],
+    projects_backend: [
+        { value: 'NOTHING_SELECTED', viewValue: 'Nothing selected' },
+        { value: 'API_GATEWAY', viewValue: 'ApiGateway' },
+        { value: 'CONFIG_MANAGER', viewValue: 'ConfigManager' },
+        { value: 'DASHBOARD', viewValue: 'Dashboard' },
+        { value: 'DATABASE_MANAGER', viewValue: 'DatabaseManager' },
+        { value: 'DATA_STORAGE_MANAGER', viewValue: 'DataStorageManager' },
+        { value: 'GAMES_MANAGER', viewValue: 'GamesManager' },
+        { value: 'HEALTH_MANAGER', viewValue: 'HealthManager' },
+        { value: 'MAMEI_FSM', viewValue: 'MaMeiFSM' },
+        { value: 'MASS_DATA_POOL', viewValue: 'MassDataPool' },
+        { value: 'SECURITY_GATEWAY', viewValue: 'SecurityGateWay' },
+        { value: 'SERVICE_REGISTRY', viewValue: 'ServiceRegistry' },
+        { value: 'MASS_DATA_POOL', viewValue: 'MassDataPool' }
+    ]
+}
