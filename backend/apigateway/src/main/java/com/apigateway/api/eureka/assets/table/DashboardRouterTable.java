57ef8b0919c5df4491fc89dbafec439f5ee8a0f8
magantus@googlemail.com <Extrem93!>
2025-02-08 00:12:37 +0100

    add route for table filter

--- backend/apigateway/src/main/java/com/apigateway/api/eureka/assets/table/DashboardRouterTable.java
+++ backend/apigateway/src/main/java/com/apigateway/api/eureka/assets/table/DashboardRouterTable.java
@@ -32,7 +32,9 @@ public class DashboardRouterTable {
 
     public static final String URI_DASH_BOARD_UPDATE_TICKET= "/ticket/";
 
-    public static final String URI_DASH_BOARD_GET_ALL_TICKET_TABLE_FILTER= "/ticket/filter/table";
+    public static final String URI_DASH_BOARD_GET_ALL_TICKET_TABLE_FILTER= "/ticket/filter/tables";
+
+    public static final String URI_DASH_BOARD_GET_CURRENT_TICKET_TABLE_FILTER= "/ticket/filter/table";
 
     public static final String URI_DASH_BOARD_GET_TICKET_TABLE_FILTER_BY_ID= "/ticket/filter/table/";
 
