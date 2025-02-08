07e5df55011a77e514771846a1b0058774d62eff
magantus@googlemail.com <Extrem93!>
2025-02-08 11:54:42 +0100

    add method wrapToTicketTableConfigDto and stringToLongList

--- backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/service/MaMeiConfigWrapperService.java
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/service/MaMeiConfigWrapperService.java
@@ -4,10 +4,12 @@ import com.configmanager.settings.frontend.projects.mamei.model.dto.MaMeiTicketT
 import com.configmanager.settings.frontend.projects.mamei.model.entity.MaMeiTicketTableConfigEntity;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.ws.rs.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MaMeiConfigWrapperService {
@@ -25,6 +27,26 @@ public class MaMeiConfigWrapperService {
                 listToString(dto.getTableFilterIds().stream().map(object -> object.toString()).toList())
         );
     }
+
+
+    public MaMeiTicketTableConfigDto wrapToTicketTableConfigDto(Optional<MaMeiTicketTableConfigEntity> entity) {
+        if (!entity.isPresent()) {
+            throw new NotFoundException("Entity not found");
+        }
+        return new MaMeiTicketTableConfigDto(
+                entity.get().getOwner(),
+                entity.get().getCurrentFilterName(),
+                entity.get().getCurrentTableId(),
+                stringToLongList(entity.get().getTableFilterIds())
+        );
+    }
+
+    private static List<Long> stringToLongList(String value) {
+        List<Long> result = new ArrayList<>();
+        stringToList(value).stream().forEach(x -> result.add(Long.valueOf(x)));
+        return result;
+    }
+
 
     private static List<String> stringToList(String value) {
         try {
