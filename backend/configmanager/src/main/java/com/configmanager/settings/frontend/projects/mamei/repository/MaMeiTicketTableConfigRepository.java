7ef307029b585fe8fb3c71ceeeebdf5fbb79ec3c
magantus@googlemail.com <Extrem93!>
2025-02-08 11:58:03 +0100

    method to find TicketConfig by Owner

--- backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/repository/MaMeiTicketTableConfigRepository.java
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/repository/MaMeiTicketTableConfigRepository.java
@@ -3,6 +3,9 @@ package com.configmanager.settings.frontend.projects.mamei.repository;
 import com.configmanager.settings.frontend.projects.mamei.model.entity.MaMeiTicketTableConfigEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MaMeiTicketTableConfigRepository extends JpaRepository<MaMeiTicketTableConfigEntity, Long> {
     void deleteByOwner(String owner);
+    Optional<MaMeiTicketTableConfigEntity>findByOwner(String owner);
 }
