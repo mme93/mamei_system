fb703ce4fa8cfaaeb6d1dcebe733a4a90f0211b9
magantus@googlemail.com <Extrem93!>
2025-02-08 23:56:24 +0100

    fix bug with unique error

--- backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/service/MaMeiTicketTableService.java
+++ backend/configmanager/src/main/java/com/configmanager/settings/frontend/projects/mamei/service/MaMeiTicketTableService.java
@@ -1,10 +1,13 @@
 package com.configmanager.settings.frontend.projects.mamei.service;
 
 import com.configmanager.settings.frontend.projects.mamei.model.dto.MaMeiTicketTableConfigDto;
+import com.configmanager.settings.frontend.projects.mamei.model.entity.MaMeiTicketTableConfigEntity;
 import com.configmanager.settings.frontend.projects.mamei.repository.MaMeiTicketTableConfigRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MaMeiTicketTableService {
@@ -31,7 +34,11 @@ public class MaMeiTicketTableService {
     public Object updateConfig(Object config) {
         ObjectMapper objectMapper = new ObjectMapper();
         MaMeiTicketTableConfigDto dto = objectMapper.convertValue(config, MaMeiTicketTableConfigDto.class);
-        return this.ticketTableConfigRepository.save(wrapperService.wrapToTicketTableConfigEntity(dto));
+        Optional<MaMeiTicketTableConfigEntity> entityOpt = ticketTableConfigRepository.findByOwner(dto.getOwner());
+        MaMeiTicketTableConfigEntity entity=entityOpt.get();
+        entity.setCurrentTableId(dto.getCurrentTableId());
+        entity.setCurrentFilterName(dto.getCurrentFilterName());
+        return this.ticketTableConfigRepository.save(entity);
     }
 
     @Transactional
