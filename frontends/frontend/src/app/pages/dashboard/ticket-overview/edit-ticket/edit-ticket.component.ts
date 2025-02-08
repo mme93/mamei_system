e3fc82976f1ec36f13b0192fc5d040032ebe39fe
magantus@googlemail.com <Extrem93!>
2025-02-08 11:53:48 +0100

    outsource dropdown data and init formControl from request

--- frontends/frontend/src/app/pages/dashboard/ticket-overview/edit-ticket/edit-ticket.component.ts
+++ frontends/frontend/src/app/pages/dashboard/ticket-overview/edit-ticket/edit-ticket.component.ts
@@ -1,7 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { tick } from '@angular/core/testing';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ticketDropDown } from 'src/app/shared/data/TicketData';
 import { TitleEventService } from 'src/app/shared/event/title-event.service';
 import { Ticket } from 'src/app/shared/model/dashboard/Ticket';
 import { TicketService } from 'src/app/shared/services/dashboard/ticket/ticket.service';
@@ -15,28 +18,13 @@ export class EditTicketComponent implements OnInit {
   ticket: Ticket | null = null;
   ticketForm: FormGroup;
   creationDate: Date = new Date();
-
-  readonly options = {
-    types: [
-      { value: 'TASK', viewValue: 'Task' },
-      { value: 'BUG', viewValue: 'Bug' },
-      { value: 'FEATURE', viewValue: 'Feature' }
-    ],
-    labels: [
-      { value: 'CODE', viewValue: 'Code' },
-      { value: 'NOTICE', viewValue: 'Notice' }
-    ],
-    classifications: [
-      { value: 'HIGH', viewValue: 'High' },
-      { value: 'MIDDLE', viewValue: 'Middle' },
-      { value: 'LOW', viewValue: 'Low' }
-    ],
-    projects : [
-      { value: 'NOTHING_SELECTED', viewValue: 'Nothing selected' },
-      { value: 'DASHBOARD', viewValue: 'Dashboard' },
-      { value: 'API_GATEWAY', viewValue: 'ApiGateway' }
-    ]
-  };
+  systems = ticketDropDown.systems;
+  types = ticketDropDown.types;
+  labels = ticketDropDown.labels;
+  classifications = ticketDropDown.classifications;
+  projects_frontend  = ticketDropDown.projects_frontend;
+  projects_backend  = ticketDropDown.projects_backend;
+  projects = this.projects_backend;
 
   constructor(
     private eventService: TitleEventService,
@@ -66,14 +54,31 @@ export class EditTicketComponent implements OnInit {
       type: ['TASK', [Validators.required]],
       label: ['NOTICE', [Validators.required]],
       classification: ['LOW', [Validators.required]],
-      projectLabel: ['NOTHING_SELECTED', [Validators.required]]
+      project: ['NOTHING_SELECTED', [Validators.required]],
+      system: ['BACKEND', [Validators.required]]
     });
   }
 
   private loadTicket(id: number): void {
     this.ticketService.getTicketById(id).subscribe(
       (ticket: Ticket) => {
+        if(ticket.system === 'FRONTEND'){
+          this.projects =this.projects_frontend;
+        }
         this.ticket = ticket;
+
+        this.ticketForm = this.fb.group({
+          title: [ticket.title, [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
+          description: [ticket.description, [Validators.required, Validators.minLength(1), Validators.maxLength(2000)]],
+          startDate: [ticket.startDate],
+          endDate: [ticket.endDate],
+          deadLine: [ticket.deadLine],
+          type: [ticket.title, [Validators.required]],
+          label: [ticket.label, [Validators.required]],
+          classification: [ticket.classification, [Validators.required]],
+          project: [ticket.project, [Validators.required]],
+          system: [ticket.system, [Validators.required]]
+        });
         this.ticketForm.patchValue(ticket);
       },
       (error: HttpErrorResponse) => {
@@ -97,4 +102,14 @@ export class EditTicketComponent implements OnInit {
       );
     }
   }
+
+   onSystemChange($event: MatSelectChange) {
+      this.projects=[];
+      this.ticketForm.get('project')?.setValue('NOTHING_SELECTED');
+        if($event.value === this.systems[0].value){
+          this.projects = this.projects_backend;
+        }else{
+          this.projects = this.projects_frontend;
+        }
+    }
 }
