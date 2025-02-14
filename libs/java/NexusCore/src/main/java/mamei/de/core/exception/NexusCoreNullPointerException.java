ccf5f43975d043d07fd86c7ffe65ca64c9c33cc6
magantus@googlemail.com <Extrem93!>
2025-02-14 11:04:03 +0100

    Exception for Nullpointer

---
+++ libs/java/NexusCore/src/main/java/mamei/de/core/exception/NexusCoreNullPointerException.java
@@ -0,0 +1,12 @@
+package mamei.de.core.exception;
+
+public class NexusCoreNullPointerException extends RuntimeException{
+
+    public NexusCoreNullPointerException(String message) {
+        super(message);
+    }
+
+    public NexusCoreNullPointerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
