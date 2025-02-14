bbef3b88c0ad9b99cd9b44e8a9cc7b81555c4009
magantus@googlemail.com <Extrem93!>
2025-02-14 11:09:31 +0100

    Method to check blank, empty and null

---
+++ libs/java/NexusCore/src/main/java/mamei/de/core/utils/check/CheckParam.java
@@ -0,0 +1,38 @@
+package mamei.de.core.utils.check;
+
+import mamei.de.core.exception.NexusCoreIsEmptyException;
+import mamei.de.core.exception.NexusCoreNullPointerException;
+
+import java.util.List;
+
+public class CheckParam {
+
+    public static Object isNotNull(Object param, String valueName) {
+        if (param == null) {
+            throw new NexusCoreNullPointerException(String.format("Param with the name %s is null.", valueName));
+        }
+        return param;
+    }
+
+    public static Object isNotBlank(Object param, String valueName) {
+        if (param == null) {
+            throw new NexusCoreNullPointerException(String.format("Param with the name %s is null.", valueName));
+        }
+
+        if (param instanceof String) {
+            if (param.toString().trim().length() == 0) {
+                throw new NexusCoreIsEmptyException(String.format("String with the name %s is empty.", valueName));
+            }
+        }
+        return param;
+    }
+
+    public static List<?> isNotEmpty(List<?> list, String listName) {
+        if (list.isEmpty()) {
+            throw new NexusCoreIsEmptyException(String.format("Param list with the name %s is empty.", listName));
+        }
+        return list;
+    }
+
+
+}
