09bde0cac0ce74ad32f6a25b4d1490703c55be24
magantus@googlemail.com <Extrem93!>
2025-02-14 11:23:04 +0100

    Core MicroService Const from System

---
+++ libs/java/NexusCore/src/main/java/mamei/de/core/model/microservice/MicroServiceConst.java
@@ -0,0 +1,12 @@
+package mamei.de.core.model.microservice;
+
+import mamei.de.core.model.microservice.enums.EMicroService;
+
+public class MicroServiceConst {
+
+    public static class ApiGateway {
+        public static final EMicroService service = EMicroService.API_GATEWAY;
+        public static final String applicationName="api";
+    }
+
+}
