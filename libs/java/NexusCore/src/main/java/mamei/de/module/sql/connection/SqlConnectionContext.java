a94eaf9f96987e1d362cbe836059aa27c8ee34a3
magantus@googlemail.com <Extrem93!>
2025-02-14 11:20:35 +0100

    Context to create a Database Connection

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/connection/SqlConnectionContext.java
@@ -0,0 +1,57 @@
+package mamei.de.module.sql.connection;
+
+import lombok.Getter;
+import lombok.Setter;
+import mamei.de.core.utils.check.CheckParam;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Getter
+@Setter
+public class SqlConnectionContext {
+
+    private String ip;
+    private String userName;
+    private String password;
+    private String serverName;
+    private String databaseName;
+
+    public SqlConnectionContext(String ip, String userName, String password, String serverName) {
+        CheckParam.isNotBlank(ip, "ip");
+        CheckParam.isNotBlank(userName, "userName");
+        CheckParam.isNotBlank(password, "password");
+        CheckParam.isNotBlank(serverName, "serverName");
+        this.ip = ip;
+        this.userName = userName;
+        this.password = password;
+        this.serverName = serverName;
+    }
+
+    public SqlConnectionContext(String ip, String userName, String password, String serverName, String databaseName) {
+        this.ip = ip;
+        this.userName = userName;
+        this.password = password;
+        this.serverName = serverName;
+        this.databaseName = databaseName;
+    }
+
+    public boolean isValidIp() {
+        try {
+            InetAddress.getByName(ip);
+            return true;
+        } catch (UnknownHostException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectionContext{" +
+                "ip='" + ip + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", serverName='" + serverName + '\'' +
+                '}';
+    }
+}
