78097e6fb3121d6af54fec92a2af36a033b6eca2
magantus@googlemail.com <Extrem93!>
2025-02-17 22:42:59 +0100

    Sql Builder to drop DatabaseElements

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/clause/drop/SqlDrop.java
@@ -0,0 +1,48 @@
+package mamei.de.module.sql.query.clause.drop;
+
+import mamei.de.core.utils.CheckParam;
+import mamei.de.module.sql.model.DatabaseElements;
+import mamei.de.module.sql.query.ISqlQuery;
+
+public class SqlDrop implements ISqlQuery {
+
+    private String content;
+    private DatabaseElements.EDatabaseElements databaseElements;
+
+    private SqlDrop(String content, DatabaseElements.EDatabaseElements databaseElements) {
+        CheckParam.isNotBlank(content, "content");
+        CheckParam.isNotNull(databaseElements, "databaseElements");
+        this.content = content;
+        this.databaseElements = databaseElements;
+    }
+
+    @Override
+    public String toSql() {
+        return String.format("DROP %s %s", databaseElements.name(), content);
+    }
+
+    @Override
+    public String getAction() {
+        return "DROP";
+    }
+
+    public static SqlDropBuilder drop() {
+        return new SqlDropBuilder();
+    }
+
+    public static class SqlDropBuilder {
+
+        private String content;
+        private DatabaseElements.EDatabaseElements databaseElements;
+
+        public SqlDropBuilder database(String databaseName) {
+            databaseElements = DatabaseElements.EDatabaseElements.DATABASE;
+            content = databaseName;
+            return this;
+        }
+
+        public SqlDrop build() {
+            return new SqlDrop(content, databaseElements);
+        }
+    }
+}
