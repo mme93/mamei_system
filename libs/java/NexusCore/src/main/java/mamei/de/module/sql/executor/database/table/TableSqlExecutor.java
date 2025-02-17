ee5e3dc3c1889a2358e120e509aafd89fe77576d
magantus@googlemail.com <Extrem93!>
2025-02-17 22:44:29 +0100

    init method for table operations

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/executor/database/table/TableSqlExecutor.java
@@ -0,0 +1,34 @@
+package mamei.de.module.sql.executor.database.table;
+
+import mamei.de.module.sql.connection.SqlConnectionContext;
+import mamei.de.module.sql.executor.AbstractSqlExecutor;
+import mamei.de.module.sql.model.ESqlEnvironment;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class TableSqlExecutor extends AbstractSqlExecutor {
+
+    public TableSqlExecutor(SqlConnectionContext connectionContext) {
+        super(connectionContext, ESqlEnvironment.TABLE);
+    }
+
+
+    public List<String> show() throws SQLException {
+        return List.of();
+    }
+
+    public boolean drop(String target) {
+        return false;
+    }
+
+
+    public boolean create(String target) {
+        return false;
+    }
+
+    public boolean alter(String target) {
+
+        return false;
+    }
+}
