5ddac137f9da8a1ae68d201c8cf1fe5f47e30d29
magantus@googlemail.com <Extrem93!>
2025-02-17 22:44:06 +0100

    fill the logic to show all databases

--- libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/clause/show/SqlShow.java
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/clause/show/SqlShow.java
@@ -1,37 +1,59 @@
 package mamei.de.module.sql.query.clause.show;
 
+import mamei.de.core.utils.CheckParam;
 import mamei.de.module.sql.query.ISqlQuery;
+
+import static mamei.de.module.sql.model.DatabaseElements.*;
 
 public class SqlShow implements ISqlQuery {
 
-    private SqlShow() {
+    private String content;
+    private EDatabaseElements databaseElements;
 
+    private SqlShow(String content, EDatabaseElements databaseElements) {
+        if(!(EDatabaseElements.DATABASES.name()==databaseElements.name())){
+            CheckParam.isNotBlank(content, "content");
+        }
+        CheckParam.isNotNull(databaseElements, "databaseElements");
+        this.content = content;
+        this.databaseElements = databaseElements;
     }
 
     @Override
     public String toSql() {
-        return "";
+        if(content!=null){
+            return String.format("SHOW %s %s", databaseElements.name(), content);
+        }
+        return String.format("SHOW %s", databaseElements.name());
     }
 
     @Override
     public String getAction() {
-        return "";
+        return "SHOW";
     }
 
-    public static SqlShowBuilder builder() {
+    public static SqlShowBuilder show() {
         return new SqlShowBuilder();
     }
 
     public static class SqlShowBuilder {
 
+        private String content;
+        private EDatabaseElements databaseElements;
 
-        public SqlShowBuilder showUserGrant() {
+        public SqlShowBuilder showDatabase() {
+            this.databaseElements = EDatabaseElements.DATABASES;
+            return this;
+        }
 
+        public SqlShowBuilder showTables(String databaseName) {
+            this.databaseElements = EDatabaseElements.DATABASE;
+            this.content=databaseName;
             return this;
         }
 
         public SqlShow build() {
-            return new SqlShow();
+            return new SqlShow(content, databaseElements);
         }
     }
 }
