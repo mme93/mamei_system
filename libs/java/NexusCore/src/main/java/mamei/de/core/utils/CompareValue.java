36946b823e2fbd52cb53045d58ea237f1665aba7
magantus@googlemail.com <Extrem93!>
2025-02-18 00:02:50 +0100

    equals to CheckValue, but without exception and return boolean

---
+++ libs/java/NexusCore/src/main/java/mamei/de/core/utils/CompareValue.java
@@ -0,0 +1,21 @@
+package mamei.de.core.utils;
+
+public class CompareValue {
+
+    public static boolean isNull(Object value) {
+        return value == null;
+    }
+
+    public static boolean isNotNull(Object value) {
+        return value == null;
+    }
+
+    public static boolean isBlank(String value) {
+        return !isNotNull(value) && value.isEmpty();
+    }
+
+    public static boolean isNotBlank(String value) {
+        return isNotNull(value) && value.isEmpty();
+    }
+
+}
