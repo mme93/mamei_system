1468a99efec936e3fada1e564c13c361ff3e8af5
magantus@googlemail.com <Extrem93!>
2025-02-19 00:00:00 +0100

    fill drop and show method with logic

--- libs/java/NexusCore/src/main/java/mamei/de/module/sql/executor/database/table/TableSqlExecutor.java
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/executor/database/table/TableSqlExecutor.java
@@ -1,25 +1,39 @@
 package mamei.de.module.sql.executor.database.table;
 
+import mamei.de.core.utils.CheckValue;
 import mamei.de.module.sql.connection.ConnectionCredentials;
 import mamei.de.module.sql.executor.AbstractSqlExecutor;
 import mamei.de.module.sql.model.ESqlEnvironment;
+import mamei.de.module.sql.query.ISqlQuery;
+import mamei.de.module.sql.query.clause.drop.SqlDrop;
+import mamei.de.module.sql.query.clause.show.SqlShow;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TableSqlExecutor extends AbstractSqlExecutor {
 
     public TableSqlExecutor(ConnectionCredentials connectionContext) {
         super(connectionContext, ESqlEnvironment.TABLE);
+        CheckValue.isNotNull(connectionContext.getDatabaseName(), "database");
     }
-
 
     public List<String> show() throws SQLException {
-        return List.of();
+        List<String> results = new ArrayList<>();
+        ISqlQuery sqlQuery = SqlShow.show().showTables().build();
+        ResultSet resultSet = executeQuery(sqlQuery);
+        while (resultSet.next()) {
+            String result = resultSet.getString(1);
+            results.add(result);
+        }
+        return results;
     }
 
-    public boolean drop(String target) {
-        return false;
+    public boolean drop(String tableName) {
+        ISqlQuery sqlQuery = SqlDrop.drop().table(tableName).build();
+        return execute(sqlQuery);
     }
 
 
@@ -32,7 +46,7 @@ public class TableSqlExecutor extends AbstractSqlExecutor {
         return false;
     }
 
-    public boolean exist(String databaseName){
+    public boolean exist(String databaseName) {
 
         return true;
     }
