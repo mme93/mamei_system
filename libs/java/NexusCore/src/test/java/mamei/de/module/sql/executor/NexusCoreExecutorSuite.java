0b75c4f7a1cb24be04ba1ba8a0535b8732a541af
magantus@googlemail.com <Extrem93!>
2025-02-20 13:21:15 +0100

    add TableSqlExecutorTest to Suite

--- libs/java/NexusCore/src/test/java/mamei/de/module/sql/executor/NexusCoreExecutorSuite.java
+++ libs/java/NexusCore/src/test/java/mamei/de/module/sql/executor/NexusCoreExecutorSuite.java
@@ -2,13 +2,15 @@ package mamei.de.module.sql.executor;
 
 import mamei.de.module.sql.executor.administration.AdministrationSqlExecutorTest;
 import mamei.de.module.sql.executor.database.DatabaseSqlExecutorTest;
+import mamei.de.module.sql.executor.database.table.TableSqlExecutorTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         AdministrationSqlExecutorTest.class,
-        DatabaseSqlExecutorTest.class
+        DatabaseSqlExecutorTest.class,
+        TableSqlExecutorTest.class
 })
 public class NexusCoreExecutorSuite {
 }
