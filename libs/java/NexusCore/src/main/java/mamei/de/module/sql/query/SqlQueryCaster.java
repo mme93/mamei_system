4c9b5bc6082b5d220657e5109bbb070e98550e50
magantus@googlemail.com <Extrem93!>
2025-02-28 09:21:18 +0100

    Object to cast different interfaces to ISqlQuery

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/SqlQueryCaster.java
@@ -0,0 +1,42 @@
+package mamei.de.module.sql.query;
+
+import mamei.de.core.exception.NexusCoreIllegalArgumentException;
+import mamei.de.module.sql.query.clause.alter.ISqlAlter;
+import mamei.de.module.sql.query.column.ISqlColumn;
+
+public class SqlQueryCaster {
+
+    public static ISqlQuery cast(Object obj) {
+        if (obj instanceof ISqlAlter) {
+            ISqlAlter alter = (ISqlAlter) obj;
+           return new CastSqlQuery(alter.toSql(), alter.getAction());
+        } else if (obj instanceof ISqlColumn) {
+            ISqlColumn column = (ISqlColumn) obj;
+            return new CastSqlQuery(column.toSql(), column.getAction());
+        } else {
+            throw new NexusCoreIllegalArgumentException(String.format("No instance found for %s",obj.getClass()));
+        }
+    }
+
+    private static class CastSqlQuery implements ISqlQuery {
+
+        private String sql;
+        private String action;
+
+        public CastSqlQuery(String sql, String action) {
+            this.sql = sql;
+            this.action = action;
+        }
+
+        @Override
+        public String toSql() {
+            return sql;
+        }
+
+        @Override
+        public String getAction() {
+            return action;
+        }
+    }
+
+}
