ff1fc9b50ed6bab930efe68cfbfc11c129401adf
magantus@googlemail.com <Extrem93!>
2025-03-02 23:40:39 +0100

    add ISqlAlter to else if

--- libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/SqlQueryCaster.java
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/SqlQueryCaster.java
@@ -9,12 +9,15 @@ public class SqlQueryCaster {
     public static ISqlQuery cast(Object obj) {
         if (obj instanceof ISqlAlter) {
             ISqlAlter alter = (ISqlAlter) obj;
-           return new CastSqlQuery(alter.toSql(), alter.getAction());
+            return new CastSqlQuery(alter.toSql(), alter.getAction());
         } else if (obj instanceof ISqlColumn) {
             ISqlColumn column = (ISqlColumn) obj;
             return new CastSqlQuery(column.toSql(), column.getAction());
+        } else if (obj instanceof ISqlAlter) {
+            ISqlAlter alter = (ISqlAlter) obj;
+            return new CastSqlQuery(alter.toSql(), alter.getAction());
         } else {
-            throw new NexusCoreIllegalArgumentException(String.format("No instance found for %s",obj.getClass()));
+            throw new NexusCoreIllegalArgumentException(String.format("No instance found for %s", obj.getClass()));
         }
     }
 
