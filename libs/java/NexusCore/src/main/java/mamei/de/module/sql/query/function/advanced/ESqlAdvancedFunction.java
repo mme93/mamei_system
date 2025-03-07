436d61b7cacbd52573e89301254720e8bd737830
magantus@googlemail.com <Extrem93!>
2025-03-07 13:56:42 +0100

    Enum for all Advance Functions

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/function/advanced/ESqlAdvancedFunction.java
@@ -0,0 +1,23 @@
+package mamei.de.module.sql.query.function.advanced;
+
+public enum ESqlAdvancedFunction {
+    BIN,
+    BINARY,
+    CASE,
+    CAST,
+    COALESCE,
+    CONNECTION_ID,
+    CONV,
+    CONVERT,
+    CURRENT_USER,
+    DATABASE,
+    IF,
+    IFNULL,
+    ISNULL,
+    LAST_INSERT_ID,
+    NULLIF,
+    SESSION_USER,
+    SYSTEM_USER,
+    USER,
+    VERSION
+}
