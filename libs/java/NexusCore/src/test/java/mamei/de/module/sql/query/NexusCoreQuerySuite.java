b2efece1f1b9c76982938677da208fc5507b25d1
magantus@googlemail.com <Extrem93!>
2025-03-07 13:04:35 +0100

    change Class in SuiteClasses

--- libs/java/NexusCore/src/test/java/mamei/de/module/sql/query/NexusCoreQuerySuite.java
+++ libs/java/NexusCore/src/test/java/mamei/de/module/sql/query/NexusCoreQuerySuite.java
@@ -1,5 +1,6 @@
 package mamei.de.module.sql.query;
 
+import mamei.de.module.sql.query.clause.NexusCoreQueryClauseSuite;
 import mamei.de.module.sql.query.privileges.SqlPrivilegesTest;
 import mamei.de.module.sql.query.user.SqlUserTest;
 import org.junit.runner.RunWith;
@@ -8,7 +9,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         SqlPrivilegesTest.class,
-        SqlUserTest.class
+        SqlUserTest.class,
+        NexusCoreQueryClauseSuite.class
 })
 public class NexusCoreQuerySuite {
 }
