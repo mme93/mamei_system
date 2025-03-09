179c763db2c6479f38e53bff386933a917a64e78
magantus@googlemail.com <Extrem93!>
2025-03-09 23:31:56 +0100

    change value name

--- libs/java/NexusCore/src/test/java/mamei/de/module/sql/executor/database/table/TableSqlExecutorTest.java
+++ libs/java/NexusCore/src/test/java/mamei/de/module/sql/executor/database/table/TableSqlExecutorTest.java
@@ -2,6 +2,7 @@ package mamei.de.module.sql.executor.database.table;
 
 import mamei.de.module.sql.executor.database.table.model.Column;
 import mamei.de.module.sql.executor.database.table.model.Table;
+import mamei.de.module.sql.executor.database.table.row.RowSqlExecutor;
 import mamei.de.module.sql.query.column.ISqlColumn;
 import mamei.de.module.sql.query.column.SqlColumnDefinition;
 import mamei.de.module.sql.query.constraints.SqlConstraint;
@@ -26,7 +27,8 @@ public class TableSqlExecutorTest {
 
     private final String tableName = "nexus_core";
 
-    private TableSqlExecutor executor = new TableSqlExecutor(mariaDBRule.CONNECTION_DB_CREDENTIALS);
+    private TableSqlExecutor tableSqlExecutor = new TableSqlExecutor(mariaDBRule.CONNECTION_DB_CREDENTIALS);
+    private RowSqlExecutor rowSqlExecutor = new RowSqlExecutor(mariaDBRule.CONNECTION_DB_CREDENTIALS);
 
     private final String ID = "ID";
     private final String NAME = "NAME";
@@ -37,29 +39,29 @@ public class TableSqlExecutorTest {
 
     @Test
     public void shouldCreateTable() throws SQLException {
-        assertFalse(executor.exist(tableName));
-        createTable(executor);
-        assertTrue(executor.exist(tableName));
-        executor.drop(tableName);
+        assertFalse(tableSqlExecutor.exist(tableName));
+        createTable(tableSqlExecutor);
+        assertTrue(tableSqlExecutor.exist(tableName));
+        tableSqlExecutor.drop(tableName);
     }
 
     @Test
     public void shouldDeleteTable() throws SQLException {
-        createTable(executor);
-        executor.drop(tableName);
-        assertFalse(executor.exist(tableName));
+        createTable(tableSqlExecutor);
+        tableSqlExecutor.drop(tableName);
+        assertFalse(tableSqlExecutor.exist(tableName));
     }
 
     @Test
     public void shouldLoadData() throws SQLException {
-        executor.setTableName(tableName);
-        createTable(executor);
-        Table table = executor.loadData();
+        tableSqlExecutor.setTableName(tableName);
+        createTable(tableSqlExecutor);
+        Table table = rowSqlExecutor.loadData();
         assertEquals(table.getTableName(), tableName);
         assertEquals(table.getRows().size(), 4);
         assertEquals(table.getDatabaseName(), mariaDBRule.CONNECTION_DB_CREDENTIALS.getDatabaseName());
         assertEquals(table.getRows().get(0).getColumns().stream().map(Column::getName).toList(),asList(ID,NAME,AGE));
-        executor.drop(tableName);
+        tableSqlExecutor.drop(tableName);
     }
 
     private void createTable(TableSqlExecutor executor) throws SQLException {
@@ -91,7 +93,7 @@ public class TableSqlExecutorTest {
                 SqlDataset.builder().addData(ID, 1L).addData(NAME, "Susi").addData(AGE, 12).build(),
                 SqlDataset.builder().addData(ID, 1L).addData(NAME, "Julian").addData(AGE, 3).build()
         );
-        executor.addDatasets(datasets, tableName);
+        rowSqlExecutor.addRows(datasets, tableName);
     }
 
 }
