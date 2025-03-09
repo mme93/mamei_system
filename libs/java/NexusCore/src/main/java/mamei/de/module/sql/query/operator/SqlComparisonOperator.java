e1cb60471711f3813a096c704535a242f6111b5c
magantus@googlemail.com <Extrem93!>
2025-03-09 23:30:03 +0100

    class with all comparisons

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/query/operator/SqlComparisonOperator.java
@@ -0,0 +1,11 @@
+package mamei.de.module.sql.query.operator;
+
+public class SqlComparisonOperator {
+    public static final String EQUALS = "=";
+    public static final String UNEQUAL = "!=";
+    public static final String GREATER_EQUAL = ">=";
+    public static final String GREATER = ">";
+    public static final String SMALLER = "<";
+    public static final String SMALLER_EQUAL = "<=";
+
+}
