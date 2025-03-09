1902e458929b08deb12cbe56980c37c619b87002
magantus@googlemail.com <Extrem93!>
2025-03-09 23:40:34 +0100

    add JavaDoc

--- libs/java/NexusCore/src/test/java/mamei/de/module/sql/connection/ConnectionManagerTest.java
+++ libs/java/NexusCore/src/test/java/mamei/de/module/sql/connection/ConnectionManagerTest.java
@@ -9,11 +9,30 @@ import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
+/**
+ * Unit tests for {@link ConnectionManager}.
+ * This class tests the connection handling of the ConnectionManager, ensuring that
+ * database connections are properly opened and closed.
+ */
 public class ConnectionManagerTest {
 
+    /**
+     * JUnit rule for setting up a MariaDB database instance.
+     * This provides the necessary database environment for testing.
+     */
     @ClassRule
     public static MariaDBRule mariaDBRule = new MariaDBRule();
 
+    /**
+     * Tests whether a database connection is properly closed after being used.
+     * It verifies that:
+     * <ul>
+     *     <li>The connection is initially open</li>
+     *     <li>The connection is closed after calling {@code closeConnection()}</li>
+     * </ul>
+     *
+     * @throws SQLException if an error occurs while handling the database connection
+     */
     @Test
     public void shouldCloseConnection() throws SQLException {
         ConnectionManager connectionManager = ConnectionManager.getInstance(MariaDBRule.CONNECTION_CREDENTIALS);
@@ -22,6 +41,4 @@ public class ConnectionManagerTest {
         connectionManager.closeConnection();
         assertTrue(con.isClosed());
     }
-
-
 }
--- libs/java/NexusCore/src/test/java/mamei/de/module/sql/executor/administration/AdministrationSqlExecutorTest.java
+++ libs/java/NexusCore/src/test/java/mamei/de/module/sql/executor/administration/AdministrationSqlExecutorTest.java
@@ -8,20 +8,46 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
+/**
+ * Unit tests for {@link AdministrationSqlExecutor}.
+ * This class tests the creation, deletion, and privilege management of system users in the database.
+ */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AdministrationSqlExecutorTest {
 
+    /**
+     * JUnit rule for setting up a MariaDB database instance.
+     * This provides the necessary database environment for testing.
+     */
     @ClassRule
     public static MariaDBRule mariaDBRule = new MariaDBRule();
 
+    /**
+     * Test user used for database operations.
+     */
     private SystemUser testUser = new SystemUser("testUser", "localhost", null);
+
+    /**
+     * Default password for the test user.
+     */
     private String password = "123";
 
+    /**
+     * Tests the creation and deletion of a system user.
+     * It verifies that:
+     * <ul>
+     *     <li>The number of users increases after creation</li>
+     *     <li>The new user exists in the user list</li>
+     *     <li>The user can be successfully deleted</li>
+     * </ul>
+     *
+     * @throws SQLException if an error occurs while interacting with the database
+     */
     @Test
     public void shouldCreateAndDeleteUser() throws SQLException {
         AdministrationSqlExecutor executor = new AdministrationSqlExecutor(MariaDBRule.CONNECTION_CREDENTIALS);
@@ -35,20 +61,29 @@ public class AdministrationSqlExecutorTest {
         assertEquals(result, 0);
     }
 
+    /**
+     * Tests the creation of a system user with all privileges.
+     * It verifies that:
+     * <ul>
+     *     <li>The user is created with default privileges</li>
+     *     <li>All privileges can be granted to the user</li>
+     *     <li>The user has exactly 38 privileges after granting</li>
+     * </ul>
+     *
+     * @throws SQLException if an error occurs while interacting with the database
+     */
     @Test
     public void shouldCreateUserWithAllPrivileges() throws SQLException {
-        String database="*";
+        String database = "*";
         AdministrationSqlExecutor executor = new AdministrationSqlExecutor(MariaDBRule.CONNECTION_CREDENTIALS);
         executor.createSystemUser(testUser, password);
         List<String> userPrivileges = executor.getGrantFromSystemUser(testUser)
                 .get(String.format("'%s'@'%s'", testUser.getName(), testUser.getHost()));
-        assertEquals(userPrivileges.size(),1);
-        assertEquals(userPrivileges.get(0),"USAGE");
-        executor.grantSystemUserToDatabase( SqlPrivileges.builder().grantAll().build(),testUser,database);
+        assertEquals(userPrivileges.size(), 1);
+        assertEquals(userPrivileges.get(0), "USAGE");
+        executor.grantSystemUserToDatabase(SqlPrivileges.builder().grantAll().build(), testUser, database);
         userPrivileges = executor.getGrantFromSystemUser(testUser)
                 .get(String.format("'%s'@'%s'", testUser.getName(), testUser.getHost()));
-        assertEquals(userPrivileges.size(),38);
+        assertEquals(userPrivileges.size(), 38);
     }
-
-
 }
