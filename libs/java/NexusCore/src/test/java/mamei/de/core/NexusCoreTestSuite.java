a313f02782ba197e56628402027c07d9c6e1e09c
magantus@googlemail.com <Extrem93!>
2025-03-09 23:22:54 +0100

    add Test-Class

--- libs/java/NexusCore/src/test/java/mamei/de/core/NexusCoreTestSuite.java
+++ libs/java/NexusCore/src/test/java/mamei/de/core/NexusCoreTestSuite.java
@@ -1,13 +1,15 @@
 package mamei.de.core;
 
 import mamei.de.core.exception.*;
-import mamei.de.core.utils.check.CheckValueTest;
+import mamei.de.core.utils.CheckValueTest;
+import mamei.de.core.utils.CompareValueTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         CheckValueTest.class,
+        CompareValueTest.class,
         NexusCoreExceptionSuite.class
 })
 public class NexusCoreTestSuite {
--- libs/java/NexusCore/src/test/java/mamei/de/module/sql/query/NexusCoreQuerySuite.java
+++ libs/java/NexusCore/src/test/java/mamei/de/module/sql/query/NexusCoreQuerySuite.java
@@ -1,6 +1,8 @@
 package mamei.de.module.sql.query;
 
 import mamei.de.module.sql.query.clause.NexusCoreQueryClauseSuite;
+import mamei.de.module.sql.query.condition.SqlConditionOperatorTest;
+import mamei.de.module.sql.query.condition.SqlConditionTest;
 import mamei.de.module.sql.query.privileges.SqlPrivilegesTest;
 import mamei.de.module.sql.query.user.SqlUserTest;
 import org.junit.runner.RunWith;
@@ -10,6 +12,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         SqlPrivilegesTest.class,
         SqlUserTest.class,
+        SqlConditionTest.class,
+        SqlConditionOperatorTest.class,
         NexusCoreQueryClauseSuite.class
 })
 public class NexusCoreQuerySuite {
