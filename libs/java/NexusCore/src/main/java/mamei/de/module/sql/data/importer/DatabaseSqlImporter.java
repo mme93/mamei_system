e19880b9e93c2c26aa647cfaeec8b25f164eba1d
magantus@googlemail.com <Extrem93!>
2025-03-12 16:06:31 +0100

    Class to import sql-tables as csv and json

---
+++ libs/java/NexusCore/src/main/java/mamei/de/module/sql/data/importer/DatabaseSqlImporter.java
@@ -0,0 +1,25 @@
+package mamei.de.module.sql.data.importer;
+
+import mamei.de.module.sql.connection.ConnectionCredentials;
+
+public class DatabaseSqlImporter {
+
+    private ConnectionCredentials connectionContext;
+
+    public DatabaseSqlImporter(ConnectionCredentials connectionContext) {
+        this.connectionContext = connectionContext;
+    }
+
+    public void importJSONFile(){
+
+    }
+
+    public void importJSONString(){
+
+    }
+
+    public void importCSV(){
+
+    }
+
+}
